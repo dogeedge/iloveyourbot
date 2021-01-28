635c935878f3e659434e357704398f3147c3b586
Vladimir Zaguljajev <zeropaz4@gmail.com>
2021-01-29 01:26:05 +0200

    Add hunting functionality

--- index.js
+++ index.js
@@ -1,77 +1,109 @@
 #! /usr/bin/env node
 
-var VanityEth = require('./libs/VanityEth');
+const VanityEth = require('./libs/VanityEth');
+const Web3 = require('web3');
 const ora = require('ora');
-var cluster = require('cluster')
-var TimeFormat = require('hh-mm-ss')
-var numCPUs = require('os').cpus().length
-var argv = require('yargs')
+const cluster = require('cluster');
+const TimeFormat = require('hh-mm-ss');
+const numCPUs = require('os').cpus().length;
+const argv = require('yargs')
     .usage('Usage: $0 <command> [options]')
     .example('$0 -checksum -i B00B5', 'get a wallet where address matches B00B5 in checksum format')
     .example('$0 --contract -i ABC', 'get a wallet where 0 nonce contract address matches the vanity')
     .example('$0 -n 25 -i ABC', 'get 25 vanity wallets')
     .example('$0 -n 1000', 'get 1000 random wallets')
+    .example('$0 --hunt', 'find wallets with balance')
+
     .alias('i', 'input')
     .string('i')
     .describe('i', 'input hex string')
+
     .alias('c', 'checksum')
     .boolean('c')
     .describe('c', 'check against the checksum address')
+
     .alias('n', 'count')
     .number('n')
     .describe('n', 'number of wallets')
+
     .boolean('contract')
     .describe('contract', 'contract address for contract deployment')
+
+    .boolean('hunt')
+    .describe('find wallets with balance')
+
+    .alias('f', 'forever')
+    .boolean('f')
+    .describe('f', 'generate/hunt addresses without limit')
+
     .alias('l', 'log')
     .boolean('l')
     .describe('l', 'log output to file')
+
     .help('h')
     .alias('h', 'help')
+
     .epilog('copyright 2018')
     .argv;
+
+let vanity = VanityEth(new Web3.providers.WebsocketProvider('wss://mainnet.infura.io/ws/v3/a29311a4546b4186affbe268fe84ebd3'));
+
 if (cluster.isMaster) {
     const args = {
         input: argv.input ? argv.input : '',
         isChecksum: argv.checksum ? true : false,
-        numWallets: argv.count ? argv.count : 1,
+        numWallets: argv.forever ? Infinity : (argv.count ? argv.count : 1),
         isContract: argv.contract ? true : false,
+        hunt: argv.hunt ? true : false,
         log: argv.log ? true : false,
         logFname: argv.log ? 'VanityEth-log-' + Date.now() + '.txt' : ''
     }
-    if (!VanityEth.isValidHex(args.input)) {
+
+    if (!vanity.isValidHex(args.input)) {
         console.error(args.input + ' is not valid hexadecimal');
         process.exit(1);
     }
     if (args.log) {
-        var fs = require('fs');
+        const fs = require('fs');
         console.log('logging into ' + args.logFname);
-        var logStream = fs.createWriteStream(args.logFname, { 'flags': 'a' });
+        var logStream = fs.createWriteStream(args.logFname, {'flags': 'a'});
     }
-    var walletsFound = 0;
-    const spinner = ora('generating vanity address 1/' + args.numWallets).start();
+    let walletsFound = 0;
+    const spinner = ora(`${args.hunt ? 'hunting' : 'generating'} vanity address 1/${args.numWallets}`).start();
     let addps = 0;
-    setInterval(function(){
-        spinner.text ='Approximate ETA for an account ' + TimeFormat.fromS((Math.pow(16,20)/Math.pow(16,20-args.input.length))/addps, 'hh:mm:ss');
+    !args.hunt && setInterval(function () {
+        spinner.text = 'Approximate ETA for an account ' + TimeFormat.fromS((Math.pow(16, 20) / Math.pow(16, 20 - args.input.length)) / addps, 'hh:mm:ss');
         addps = 0;
-    },1000)
-    for (var i = 0; i < numCPUs; i++) {
+    }, 1000)
+    for (let i = 0; i < numCPUs; i++) {
         const worker_env = {
             input: args.input,
             isChecksum: args.isChecksum,
-            isContract: args.isContract
+            isContract: args.isContract,
+            hunt: args.hunt
         }
         proc = cluster.fork(worker_env);
-        proc.on('message', function(message) {
-            if(message.account){
+        proc.on('message', function (message) {
+            if (message.balance) {
                 spinner.succeed(JSON.stringify(message));
                 if (args.log) logStream.write(JSON.stringify(message) + "\n");
-                    walletsFound++;
+                walletsFound++;
                 if (walletsFound >= args.numWallets) {
                     cleanup();
                 }
-                spinner.text ='generating vanity address ' + (walletsFound + 1)  +'/' + args.numWallets;
+                spinner.text = 'hunting vanity address ' + (walletsFound + 1) + '/' + args.numWallets;
                 spinner.start();
-            } else if(message.counter){
+            }
+            else if (message.account) {
+                spinner.succeed(JSON.stringify(message));
+                if (args.log) logStream.write(JSON.stringify(message) + "\n");
+                walletsFound++;
+                if (walletsFound >= args.numWallets) {
+                    cleanup();
+                }
+                spinner.text = 'generating vanity address ' + (walletsFound + 1) + '/' + args.numWallets;
+                spinner.start();
+            } else if (message.counter) {
                 addps++
             }
         });
@@ -79,19 +111,32 @@ if (cluster.isMaster) {
 
 } else {
     const worker_env = process.env;
-    while (true) {
-        process.send({
-            account: VanityEth.getVanityWallet(worker_env.input, worker_env.isChecksum == 'true', worker_env.isContract == 'true', function (){
+
+    if (worker_env.hunt == 'true') while (true) {
+        const account = vanity.getVanityWallet(worker_env.input, worker_env.isChecksum == 'true', worker_env.isContract == 'true', function () {
             process.send({
                 counter: true
             })
-        })})
+        });
+        const balance = vanity.getWalletBalance(account.address);
+
+        if (balance > 0) process.send({
+            account: account,
+            balance: balance
+        })
     }
+    else while (true) process.send({
+        account: vanity.getVanityWallet(worker_env.input, worker_env.isChecksum == 'true', worker_env.isContract == 'true', function () {
+            process.send({
+                counter: true
+            })
+        })
+    })
 }
 process.stdin.resume();
-var cleanup = function(options, err) {
+var cleanup = function (options, err) {
     if (err) console.log(err.stack);
-    for (var id in cluster.workers) cluster.workers[id].process.kill();
+    for (let id in cluster.workers) cluster.workers[id].process.kill();
     process.exit();
 }
 process.on('exit', cleanup.bind(null, {}));
--- libs/VanityEth.js
+++ libs/VanityEth.js
@@ -1,46 +1,83 @@
 const crypto = require('crypto');
-var ethUtils = require('ethereumjs-util');
-var ERRORS = {
-    invalidHex: "Invalid hex input"
-}
-var getRandomWallet = function() {
-    var randbytes = crypto.randomBytes(32);
-    var address = '0x' + ethUtils.privateToAddress(randbytes).toString('hex');
-    return { address: address, privKey: randbytes.toString('hex') }
-}
-var isValidHex = function(hex) {
+const ethUtils = require('ethereumjs-util');
+const Web3 = require('web3');
+
+const ERRORS = {
+    invalidHex: "Invalid hex input",
+    invalidAddress: "Invalid address"
+};
+const getRandomWallet = function () {
+    const randbytes = crypto.randomBytes(32);
+    const address = '0x' + ethUtils.privateToAddress(randbytes).toString('hex');
+    return {address: address, privKey: randbytes.toString('hex')}
+};
+const isValidHex = function (hex) {
     if (!hex.length) return true;
     hex = hex.toUpperCase();
-    var re = /^[0-9A-F]+$/g;
+    const re = /^[0-9A-F]+$/g;
     return re.test(hex);
-}
-var isValidVanityWallet = function(wallet, input, isChecksum, isContract) {
-    var _add = wallet.address;
+};
+const getDeterministicContractAddress = function (address) {
+    return '0x' + ethUtils.sha3(ethUtils.rlp.encode([address, 0])).slice(12).toString('hex');
+};
+const isValidVanityWallet = function (wallet, input, isChecksum, isContract) {
+    let _add = wallet.address;
     if (isContract) {
-        var _contractAdd = getDeterministicContractAddress(_add);
+        let _contractAdd = getDeterministicContractAddress(_add);
         _contractAdd = isChecksum ? ethUtils.toChecksumAddress(_contractAdd) : _contractAdd;
         wallet.contract = _contractAdd;
         return _contractAdd.substr(2, input.length) == input
     }
     _add = isChecksum ? ethUtils.toChecksumAddress(_add) : _add;
     return _add.substr(2, input.length) == input;
-}
-var getVanityWallet = function(input = '', isChecksum = false, isContract = false, counter = function(){}) {
+};
+const getVanityWallet = function (input = '', isChecksum = false, isContract = false, counter = function () {
+}) {
     if (!isValidHex(input)) throw new Error(ERRORS.invalidHex);
     input = isChecksum ? input : input.toLowerCase();
-    var _wallet = getRandomWallet();
+    let _wallet = getRandomWallet();
     while (!isValidVanityWallet(_wallet, input, isChecksum, isContract)) {
         counter()
         _wallet = getRandomWallet(isChecksum);
     }
     if (isChecksum) _wallet.address = ethUtils.toChecksumAddress(_wallet.address);
     return _wallet;
-}
-var getDeterministicContractAddress = function(address) {
-    return '0x' + ethUtils.sha3(ethUtils.rlp.encode([address, 0])).slice(12).toString('hex');
-}
-module.exports = {
-    getVanityWallet: getVanityWallet,
-    isValidHex: isValidHex,
-    ERRORS: ERRORS
+};
+const getWalletBalanceFactory = function(web3) {
+    return function(address) {
+        if (!ethUtils.isValidAddress(address)) throw new Error(ERRORS.invalidAddress);
+
+        let balance = null;
+
+        web3.eth.getBalance(address, function (err, result) {
+            if (err) {
+                balance = NaN;
+            } else {
+                balance = +web3.utils.fromWei(result, 'ether');
+            }
+        });
+
+        while (balance === null) {
+            // wait for balance
+        }
+
+        return balance;
+    };
+};
+
+module.exports = function (web3Provider) {
+    if (!(
+        web3Provider instanceof Web3.providers.HttpProvider ||
+        web3Provider instanceof Web3.providers.IpcProvider ||
+        web3Provider instanceof Web3.providers.WebsocketProvider
+    )) throw new TypeError('web3Provider is of invalid type');
+
+    const web3 = new Web3(web3Provider)
+
+    return {
+        getVanityWallet,
+        getWalletBalance: getWalletBalanceFactory(web3),
+        isValidHex,
+        ERRORS
+    }
 }
